6d26c8687f6041b36293c4d2544b120eb1104324
Harsh <harshkulkarni78@gmail.com>
2023-11-03 13:52:01 +0530

    add categories automatically by inserting data

--- src/com/database/Category.java
+++ src/com/database/Category.java
@@ -1,5 +1,6 @@
 package com.database;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,6 +13,22 @@ public class Category extends Database {
             Statement statement = getStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS \"expense_types\" (" + "\t\"catagary_id\"\tINTEGER," + "\t\"category\"\tTEXT NOT NULL UNIQUE," + "\tPRIMARY KEY(\"catagary_id\" AUTOINCREMENT)" + ")");
             statement.close();
+
+            boolean dataExist = true;
+            Statement statement2 = getStatement();
+            ResultSet resultSet = statement2.executeQuery("SELECT \"category\" FROM expense_types");
+            if (resultSet.getInt(1) == 0) {
+                dataExist = false;
+            }
+            statement2.close();
+            String[] categories = new String[]{"Personal expense", "Shopping", "Food and dinning", "Travelling", "Entrainment", "Medical", "Education", "Bill and Utilities", "Investment", "Taxes", "Insurance", "Other"};
+            if (!dataExist) {
+                PreparedStatement preparedStatement = getPrepareStatement("INSERT INTO expense_types " + "(\"category\")" + "VALUES (?)");
+                for (String category : categories) {
+                    preparedStatement.setString(1, category);
+                }
+                preparedStatement.close();
+            }
         } catch (SQLException e) {
             // ignore
         }
