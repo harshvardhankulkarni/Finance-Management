283bd702d82aa56afe8f983db9f24006d1400b85
Harsh <harshkulkarni78@gmail.com>
2023-11-05 19:55:54 +0530

    automatically add category when table created

--- src/com/database/Category.java
+++ src/com/database/Category.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Category extends Database {
 
@@ -14,20 +15,21 @@ public class Category extends Database {
             statement.execute("CREATE TABLE IF NOT EXISTS \"expense_types\" (" + "\t\"catagary_id\"\tINTEGER," + "\t\"category\"\tTEXT NOT NULL UNIQUE," + "\tPRIMARY KEY(\"catagary_id\" AUTOINCREMENT)" + ")");
             statement.close();
 
-            boolean dataExist = true;
+            boolean dataExist = false;
             Statement statement2 = getStatement();
             ResultSet resultSet = statement2.executeQuery("SELECT \"category\" FROM expense_types");
-            if (resultSet.getString(1).isEmpty()) {
-                dataExist = false;
+            if (resultSet.next()) {
+                dataExist = true;
             }
             statement2.close();
             String[] categories = new String[]{"Personal expense", "Shopping", "Food and dinning", "Travelling", "Entrainment", "Medical", "Education", "Bill and Utilities", "Investment", "Taxes", "Insurance", "Other"};
             if (!dataExist) {
-                PreparedStatement preparedStatement = getPrepareStatement("INSERT INTO expense_types " + "(\"category\")" + "VALUES (?)");
                 for (String category : categories) {
+                    PreparedStatement preparedStatement = getPrepareStatement("INSERT INTO expense_types " + "(\"category\")" + "VALUES (?)");
                     preparedStatement.setString(1, category);
+                    preparedStatement.executeUpdate();
+                    preparedStatement.close();
                 }
-                preparedStatement.close();
             }
         } catch (SQLException e) {
             // ignore
